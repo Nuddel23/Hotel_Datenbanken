a46cee58529ce46d42a18a2ddf3cad10121e664d
Kiza <john.schingarjow@proton.me>
2024-11-14 13:30:27 +0100

    Projektdateien hinzufügen.

---
+++ Hotel_Datenbanken/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MySqlConnector;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel_Datenbanken
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connection();
+        }
+
+        public void Connection()
+        {
+            using (var connection = new MySqlConnection("Server=localhost; User ID = root; Password = ; Database = hotel")) 
+            { 
+                connection.Open();
+
+                using (var command = new MySqlCommand("SELECT * FROM gast; ",connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.GetInt32(0) == 1) { 
+                            test.Text = reader.GetString(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
