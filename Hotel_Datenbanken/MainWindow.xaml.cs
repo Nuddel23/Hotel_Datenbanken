df67f724ea67e27aabcd9da6c588b8d5d2111a30
Nuddel23 <john.schingarjow@web.de>
2024-11-28 12:30:09 +0100

    tabs erstellt

--- Hotel_Datenbanken/MainWindow.xaml.cs
+++ Hotel_Datenbanken/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MySqlConnector;
+using System.Data;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,29 +19,75 @@ namespace Hotel_Datenbanken
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySqlConnection DB;
         public MainWindow()
         {
             InitializeComponent();
+            DB = new MySqlConnection("Server=localhost; User ID = root; Password = ; Database = test");
+            DB.Open();
             Connection();
         }
 
         public void Connection()
         {
-            using (var connection = new MySqlConnection("Server=localhost; User ID = root; Password = ; Database = hotel")) 
-            { 
-                connection.Open();
+            var command = new MySqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='test'", DB);
 
-                using (var command = new MySqlCommand("SELECT * FROM gast; ",connection))
-                using (var reader = command.ExecuteReader())
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
                 {
-                    while (reader.Read())
-                    {
-                        if (reader.GetInt32(0) == 1) { 
-                            test.Text = reader.GetString(1);
-                        }
-                    }
+                    combobox.Items.Add(reader.GetString(0));
+                    //test.Text = reader.GetString(i);
                 }
             }
+        }
+
+        private void combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void LoadTable(string tablename)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (var command = new MySqlCommand($"SELECT * FROM {tablename}; ", DB))
+            {
+                using (var adapter = new MySqlDataAdapter(command))
+                {
+                    adapter.Fill(dataTable);
+                }
+            }
+            tabelle.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void combobox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void combobox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+            DataTable GastTablle = new DataTable();
+
+            using (var command = new MySqlCommand($"SELECT * FROM " + combobox.SelectedValue + "; ", DB))
+            {
+                using (var adapter = new MySqlDataAdapter(command))
+                {
+                    adapter.Fill(GastTablle);
+                }
+            }
+            tabelle.ItemsSource = GastTablle.DefaultView;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tabcontrol.SelectedIndex = 1;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            tabcontrol.SelectedIndex = 0;
         }
     }
 }
